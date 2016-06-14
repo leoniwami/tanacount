cdd9d30767b97e2fc90df2cbc6c25955eacfdf39
leoniwami <leoniwamidragon@gmail.com>
2016-06-14 20:35:46 +0900

    眠い

--- lookingfor-2ndproject/CameraViewController.swift
+++ lookingfor-2ndproject/CameraViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
@@ -6,8 +7,36 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBOutlet var takepicture : UIButton!
     @IBOutlet var savepicture : UIButton!
     
+    // file操作をするときに役立つNSFileManager
+    // 録音したファイルをDocmentsディレクトリに保存しています。
+    // 他にもtmpディレクトリやLibrary/Cachesディレクトリなんかがあるので、
+    // 興味あればググってください
+    let fileManager = NSFileManager()
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
+    let fileName = "sample.caf"
+    var monoURL: NSURL!
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var pauseButton: UIButton!
+    @IBOutlet var stopButton: UIButton!
+    
+    @IBOutlet var textforthing: UITextField!
+    
+    struct information {
+        var images: UIImage?
+        var textmessages: String?
+        var recordmessages: NSURL?
+    }
+    
+//    var mono: information!
+//    var momo = information()
+    var monoArray = [information]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupAudioRecorder()
     }
     
     // カメラの撮影開始
@@ -45,16 +74,28 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     // 写真を保存
     @IBAction func savePic(sender : AnyObject) {
-        let image:UIImage! = cameraView.image
+        var mono1 = information()
+        let image: UIImage? = cameraView.image
+        print(image)
         
         if image != nil {
-            UIImageWriteToSavedPhotosAlbum(image, self, #selector(CameraViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            // iphoneのアルバムに保存
+            UIImageWriteToSavedPhotosAlbum(image!, self, #selector(CameraViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            
+            //
+            mono1.images = cameraView.image!
         }
+    
+        mono1.textmessages = textforthing.text
+        mono1.recordmessages = monoURL
+        
+        monoArray.append(mono1)
+        print(monoArray)
     }
     
     // 書き込み完了結果の受け取り
     func image(image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutablePointer<Void>) {
-        print("1")
+        print("書き込み完了結果の受け取り")
         
     }
     
@@ -68,6 +109,62 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
             cameraPicker.delegate = self
             self.presentViewController(cameraPicker, animated: true, completion: nil)
         }
+    }
+    
+    // 録音ボタンを押した時の挙動
+    @IBAction func pushRecordButton(sender: AnyObject) {
+        audioRecorder?.record()
+    }
+    
+    @IBAction func pushPauseButton(sender: AnyObject) {
+        audioRecorder?.pause()
+    }
+    
+    @IBAction func pushStopButton(sender: AnyObject) {
+        audioRecorder?.stop()
+    }
+    
+    // 再生ボタンを押した時の挙動
+    @IBAction func pushPlayButton(sender: AnyObject) {
+        self.play()
+    }
+    
+    // 録音するために必要な設定を行う
+    // viewDidLoad時に行う
+    func setupAudioRecorder() {
+        // 再生と録音機能をアクティブにする
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try! session.setActive(true)
+        let recordSetting : [String : AnyObject] = [
+            AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey : 16,
+            AVNumberOfChannelsKey: 2,
+            AVSampleRateKey: 44100.0
+        ]
+        do {
+            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+        } catch {
+            print("初期設定でerror出たよ(-_-;)")
+        }
+    }
+    // 再生
+    func play() {
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: self.documentFilePath())
+        } catch {
+            print("再生時にerror出たよ(´・ω・｀)")
+        }
+        audioPlayer?.play()
+        
+    }
+    // 録音するファイルのパスを取得(録音時、再生時に参照)
+    // swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
+    func documentFilePath()-> NSURL {
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let dirURL = urls[0]
+        monoURL = dirURL
+        return dirURL.URLByAppendingPathComponent(fileName)
     }
     
     override func didReceiveMemoryWarning() {
